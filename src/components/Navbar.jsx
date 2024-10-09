92777c288b79322fabfe0e0dd491c2e832c9ed5e
Joel Josafat Hernández Saucedo <jojohersa21@gmail.com>
2024-10-08 23:55:31 -0600

    add navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,41 @@
+import "../styles/navbar.css";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar-container left">
+      <div className="navbar-links_container">
+        <ul>
+          <li>
+            <a href="/">Inicio</a>
+          </li>
+          <li>
+            <a href="/rutinas">Rutinas</a>
+          </li>
+          <li>
+            <a href="/asistente">Asistente virtual</a>
+          </li>
+        </ul>
+      </div>
+
+      <div className="navbar-img">
+        <img src="/logo.png" alt="Logo" className="logo" />
+      </div>
+
+      <div className="navbar-links_container right">
+        <ul>
+          <li>
+            <a href="/plan">Planes</a>
+          </li>
+          <li>
+            <a href="/nosotros">Nosotros</a>
+          </li>
+          <li>
+            <a href="/login">Conectarse</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,8 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
+import Layout from "./layout/Layout";
+import Home from "./views/Home/Home";
 
 const router = createBrowserRouter([
   {
     path: "/",
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+    ],
   },
 ]);
 
