fdfb47f8625e52c56f57f24b036e9d7507458688
Peluchitos <un.peluchito@gmail.com>
2024-10-12 19:03:19 -0600

    Pequeños detalles de css
    Sólo moví un par de cosas y el icono de la lupa

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -23,12 +23,36 @@ const Home = () => {
       </div>
 
       <div>
-        <input
-          type="text"
-          placeholder="🔎 Search"
-          value={search}
-          onChange={handleSearch}
-        />
+      <div style={{ position: 'relative', display: 'inline-block' }}>
+  <input
+    type="text"
+    placeholder="Search"
+    value={search}
+    onChange={handleSearch}
+    style={{ paddingLeft: '30px' }} 
+  />
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      style={{
+        position: 'absolute',
+        left: '1.5rem',
+        top: '50%',
+        transform: 'translateY(-50%)',
+        width: '16px',
+        height: '16px',
+      }}
+    >
+      <circle cx="11" cy="11" r="8" />
+      <line x1="21" y1="21" x2="16.65" y2="16.65" />
+    </svg>
+  </div>
+
       </div>
     </main>
   );
