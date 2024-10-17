e3d5a3e1f6b026e8cfa42a1aa0fba3d1626effbc
Peluchitos <un.peluchito@gmail.com>
2024-10-16 21:28:48 -0600

    Rutas y rutinas
    Sólo agregué un poquito de la de rutinas para tener algo más

--- src/utils/tempDb.js
+++ src/utils/tempDb.js
@@ -1,27 +1,27 @@
 const tempDb = [
   {
     id: 1,
-    title: "Primer Blog",
+    title: "Cómo Iniciar una Rutina de Ejercicio y No Abandonar en el Intento",
     image: "/sample.png",
-    briefText: "Este es un breve resumen del primer blog.",
+    briefText: "Consejos para comenzar una rutina de ejercicio y mantenerla a largo plazo.",
     fullText:
-      "Este es el texto completo del primer blog. Aquí se puede encontrar más información detallada sobre el tema tratado en el blog.",
+      "Iniciar una rutina de ejercicio es fácil; lo difícil es mantenerla. Aquí te damos algunos consejos para que el fitness se convierta en parte de tu estilo de vida. Establece metas realistas, encuentra una actividad que disfrutes y crea un plan con un horario fijo. La constancia y la paciencia son esenciales para ver resultados, pero lo más importante es disfrutar del proceso.",
   },
   {
     id: 2,
-    title: "Segundo Blog",
+    title: "Los Beneficios de Entrenar con Pesas para Mujeres",
     image: "/sample.png",
-    briefText: "Este es un breve resumen del segundo blog.",
+    briefText: "Descubre cómo el entrenamiento con pesas puede transformar tu salud y cuerpo.",
     fullText:
-      "Este es el texto completo del segundo blog. Aquí se puede encontrar más información detallada sobre el tema tratado en el blog.",
+      "El entrenamiento con pesas no solo es para ganar músculo, sino también para mejorar la salud en general. Levantar pesas fortalece tus huesos, acelera el metabolismo y mejora la composición corporal, ayudando a esculpir y tonificar el cuerpo. Las mujeres pueden beneficiarse enormemente de incluir pesas en su rutina, y los beneficios van mucho más allá de la estética.",
   },
   {
     id: 3,
-    title: "Tercer Blog",
+    title: "Los 5 Mejores Alimentos para la Recuperación Post-Entrenamiento",
     image: "/sample.png",
-    briefText: "Este es un breve resumen del tercer blog.",
+    briefText: "Optimiza tu recuperación post-entrenamiento con estos alimentos clave.",
     fullText:
-      "Este es el texto completo del tercer blog. Aquí se puede encontrar más información detallada sobre el tema tratado en el blog.",
+      "La nutrición es clave para maximizar los resultados de tus entrenamientos. Después de entrenar, tu cuerpo necesita nutrientes específicos para reparar y construir músculo. Alimentos como el pollo a la parrilla, batidos de proteína con plátano, huevos, avena y yogur griego son excelentes opciones para acelerar la recuperación y recargar tu energía.",
   },
 ];
 
